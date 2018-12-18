cf435b264325e8e83d35d75e3a80fc48f1b292cd
Veprev Alexey <funkylex@yandex.ru>
2018-12-18 16:12:41 +0200

    cors init

--- app/app.ts
+++ app/app.ts
@@ -63,6 +63,13 @@ class App {
          */
         this.app.use(bodyParser.json());
 
+        this.app.all("/*", function(req, res, next) {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+            res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+            return next();
+        });
+
         /**
          * support application/x-www-form-urlencoded post data
          */
